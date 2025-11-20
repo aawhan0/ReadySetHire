858581fecda3001a365f480adfc2c340b8803f79
Aawhan <114390114+aawhan0@users.noreply.github.com>
2025-11-20 09:36:17 +0530

    frontend-3

--- frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // for React 18+
-import App from '.pages/App.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
